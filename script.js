6d92e167b05707f2159d711e785d429f588aeae5
Damitha Perera <damitha@keeneye.us>
2020-02-20 13:21:36 +0530

    models files added

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const video = document.getElementById('video')
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;   // browser versions
 
-function startVideo() {
-    navigator.getUserMedia(
-      { video: true },
-      stream => video.srcObject = stream,
-      err => console.error(err)
-    )
-  }
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
 
-startVideo();
+function startVideo() {
+  navigator.getUserMedia(
+    { video: true },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  console.log('lol');
+})
